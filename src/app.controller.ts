1c42201f4ad51400587e7da56ff00e2313c29387
Glenn <a810.glenn@gmail.com>
2022-10-21 19:57:52 +0900

    Request Object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Req } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -6,12 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log(req)
     return this.appService.getHello()
   }
 
-  @Get('/he*lo')
-  getHello2(): string {
-    return this.appService.getHello()
-  }
+  // @Get('/he*lo')
+  // getHello2(): string {
+  //   return this.appService.getHello()
+  // }
 }
