94027e3bc37890624bd586358fb25e118670ee2f
Glenn <a810.glenn@gmail.com>
2022-10-23 21:40:36 +0900

    request / response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Req } from '@nestjs/common'
+// import { Request } from 'express'
+import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+// import { UsersService } from './users/users.service'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // @Header('Custom', 'Test Header')
+  // @Get(':id')
+  // findOneWithHeader(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id)
+  // }
 
   @Get()
   getHello(): string {
@@ -19,5 +27,10 @@ export class AppController {
   // @Get('/he*lo')
   // getHello2(): string {
   //   return this.appService.getHello()
+  // }
+  // findAll(@Res() res) {
+  //   const users = this.usersService.findAll()
+  //
+  //   return res.status(200).send(users)
   // }
 }
